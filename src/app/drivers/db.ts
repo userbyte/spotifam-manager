a69a39b5030b71a3997961c3ab9ffef0436069c9
userbyte <42632711+userbyte@users.noreply.github.com>
2025-09-23 00:47:38 -0400

    add db connection timeout time

--- src/app/drivers/db.ts
+++ src/app/drivers/db.ts
@@ -1,6 +1,21 @@
 import { Db, MongoClient } from "mongodb";
-const connectionString = process.env.MONGO_URI || "";
-const client = new MongoClient(connectionString);
+const connectionString =
+  process.env.MONGO_URI || "mongodb://127.0.0.1:27017/placeholder";
+if (!process.env.MONGO_URI) {
+  console.error(
+    "MONGO_URI env var is unset. this is required... please make sure its set!"
+  );
+  console.error(
+    "MONGO_URI env var is unset. this is required... please make sure its set!"
+  );
+  console.error(
+    "MONGO_URI env var is unset. this is required... please make sure its set!"
+  );
+  console.error(
+    "MONGO_URI env var is unset. this is required... please make sure its set!"
+  );
+}
+const client = new MongoClient(connectionString, { timeoutMS: 10000 });
 
 let conn: MongoClient | null = null;
 let db: Db | null = null;
