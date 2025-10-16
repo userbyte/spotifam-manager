6f9fc780278859c54dcb1bbe16a03cd8b3f2ec02
userbyte <42632711+userbyte@users.noreply.github.com>
2025-10-16 00:15:08 -0400

    implement JoinFamily component

--- src/app/components/JoinFamily.tsx
+++ src/app/components/JoinFamily.tsx
@@ -1,10 +1,40 @@
+"use client";
+import { useRef } from "react";
+import { useRouter } from "next/navigation";
 import styles from "@/app/style/module/JoinFamily.module.css";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default async function JoinFamily() {
+export default function JoinFamily() {
+  const router = useRouter();
+  const familyCodeInput = useRef<HTMLInputElement | null>(null);
+
+  function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (familyCodeInput.current && e.key === "Enter") {
+      router.replace(`/family?code=${familyCodeInput.current.value}`);
+    }
+  }
+
+  function handleClick() {
+    if (familyCodeInput.current) {
+      router.replace(`/family?code=${familyCodeInput.current.value}`);
+    }
+  }
+
   return (
     <div className={styles.main}>
       <h1>Enter an existing family code</h1>
-      <input type="text" placeholder="Enter a code..."></input>
+      <span>
+        <input
+          ref={familyCodeInput}
+          onKeyDown={handleKeyDown}
+          type="text"
+          placeholder="Enter a code..."
+        ></input>
+        <button onClick={handleClick}>
+          <FontAwesomeIcon icon={faCheck} />
+        </button>
+      </span>
     </div>
   );
 }
